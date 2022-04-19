755bca6ad6106e79b40350ce242cd7e60609d2ca
pvicugna11 <pvicugna11@gmail.com>
2022-04-19 10:46:36 +0900

    鏡

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -7,12 +7,11 @@ public class Mirror : MonoBehaviour
     public Camera TrackingCamera;
     public Camera ReflectionCamera;
     public Transform Specular;
+    public Transform Frame;
     public float Size = 10;
 
     void Update()
     {
-        Specular.localScale = new Vector3(-Size, Size, 1);
-
         var diff = transform.position - TrackingCamera.transform.position;
         var normal = transform.forward;
         var reflection = diff + 2 * (Vector3.Dot(-diff, normal)) * normal;
@@ -21,9 +20,14 @@ public class Mirror : MonoBehaviour
         ReflectionCamera.transform.LookAt(Specular.position);
 
         var distance = Vector3.Distance(transform.position, ReflectionCamera.transform.position);
-        ReflectionCamera.nearClipPlane = distance * 0.9f;
+        ReflectionCamera.nearClipPlane = distance * .9f;
 
-        ReflectionCamera.fieldOfView = 2 * Mathf.Atan(Size / (2 * distance)) * Mathf.Rad2Deg;
+        Frame.localScale = new Vector3(Size, Size, 1);
+        var angle = Vector3.Angle(-transform.forward, ReflectionCamera.transform.forward);
+        var specularSize = Size / Mathf.Abs(Mathf.Cos(angle * Mathf.Deg2Rad)) * 2f;
+        Specular.localScale = new Vector3(-specularSize, specularSize, 1);
+
+        ReflectionCamera.fieldOfView = 2 * Mathf.Atan(specularSize / (2 * distance)) * Mathf.Rad2Deg;
 
         Specular.LookAt(diff);
     }
