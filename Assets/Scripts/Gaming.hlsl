1edd62c3311168fb0b65531e8168c8c2cb273240
pvicugna11 <pvicugna11@gmail.com>
2022-08-28 18:25:56 +0900

    Delete

--- Assets/Scripts/Gaming.hlsl
+++
@@ -1,9 +0,0 @@
-void Gaming_float(float time, float term, uint num, out float value)
-{
-    value = 1 / 2;
-
-    for (uint i = 0; i < num; i++)
-    {
-        value += 12 * cos(((2 * i + 1) * PI * time) / (3 * term)) / pow((2 * i + 1) * PI, 2);
-    }
-}
